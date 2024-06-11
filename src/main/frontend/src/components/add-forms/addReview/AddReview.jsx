6d54e71c920e60c59e06d230a41b5f4bb3f0c1b9
Santos Bogo <72747461+santosbogo@users.noreply.github.com>
2024-06-11 11:25:27 -0300

    Limits of chaeracters in reviews

--- src/main/frontend/src/components/add-forms/addReview/AddReview.jsx
+++ src/main/frontend/src/components/add-forms/addReview/AddReview.jsx
@@ -1,19 +1,18 @@
-import React, {useState, useRef, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import './AddReview.css';
 import axios from "axios";
 import withAuth from "../../hoc/withAuth";
-import {Button, IconButton, TextField} from '@mui/material';
+import { Button, IconButton, TextField } from '@mui/material';
 import CloseIcon from '@mui/icons-material/Close';
 import Mention from "./Mention";
 import ReactMarkdown from "react-markdown";
 
 const AddReview = React.forwardRef(({ showTitle, onRemove }, ref) => {
-
     const [review, setReview] = useState('');
     const [rating, setRating] = useState('');
     const [isSearching, setIsSearching] = useState(false);
     const [myMap, setMyMap] = useState({});
-
+    const [errorMessage, setErrorMessage] = useState('');
 
     const handleSubmit = async (event) => {
         event.preventDefault();
@@ -30,8 +29,8 @@ const AddReview = React.forwardRef(({ showTitle, onRemove }, ref) => {
                 }
             });
 
-            console.log("res")
-            console.log(response.data)
+            console.log("res");
+            console.log(response.data);
             if (response.status === 200) {
                 onRemove();
                 //window.location.reload(); // malardo esto
@@ -44,13 +43,18 @@ const AddReview = React.forwardRef(({ showTitle, onRemove }, ref) => {
 
     const handleTextChange = (event) => {
         const text = event.target.value;
-        if (text.charAt(text.length - 1) === '@'){
-            setIsSearching(true);
+        if (text.length > 1000) {
+            setErrorMessage('Review cannot exceed 1000 characters');
+        } else {
+            setErrorMessage('');
+            if (text.charAt(text.length - 1) === '@') {
+                setIsSearching(true);
+            }
+            if (!isSearching) {
+                setReview(text);
+            }
         }
-        if (!isSearching) {
-            setReview(text);
-        }
-    }
+    };
 
     const markdownTextifier = async () => {
         return new Promise((resolve) => {
@@ -66,55 +70,56 @@ const AddReview = React.forwardRef(({ showTitle, onRemove }, ref) => {
 
             resolve(updatedReview);
         });
-    }
+    };
 
     const handleSelectedResult = (result) => {
         const texto = `@[${result.name}](http://localhost:3000/${result.type}/${result.id})`;
         setReview(review + `${result.name}`);
-        setMyMap(prevMap => ({...prevMap, [result.name]: texto}));
+        setMyMap(prevMap => ({ ...prevMap, [result.name]: texto }));
         setIsSearching(false);
     };
 
-return (
-    <div ref={ref} className="add-review-container">
-        <IconButton className={"close-button"} onClick={onRemove}>
-            <CloseIcon/>
-        </IconButton>
-        <form onSubmit={handleSubmit}>
-            <div className="rating">
-                {[...Array(5)].map((star, index) => {
-                    const ratingValue = 5 - index;
-                    return (
-                        <label key={index} className={ratingValue <= rating ? "selected" : ""}>
-                            <input
-                                type="radio"
-                                name="rating"
-                                value={ratingValue}
-                                onClick={() => setRating(ratingValue)}
-                            />
-                            &#9733; {/* Star character */}
-                        </label>
-                    );
-                })}
-            </div>
-            <TextField
-                placeholder={"Write your review..."}
-                className="review-input"
-                multiline={true}
-                onChange={handleTextChange}
-                value={review}
-            />
-            <Button variant="contained"
-                    type="submit"
-                    color="primary"
-                    disabled={!(review && rating)}>
-                Submit Review
-            </Button>
-        </form>
-        <Mention onResultSelect={handleSelectedResult}
-                 isSearching={isSearching}
-                 setIsSearching={setIsSearching}/>
-    </div>
+    return (
+        <div ref={ref} className="add-review-container">
+            <IconButton className={"close-button"} onClick={onRemove}>
+                <CloseIcon />
+            </IconButton>
+            <form onSubmit={handleSubmit}>
+                <div className="rating">
+                    {[...Array(5)].map((star, index) => {
+                        const ratingValue = 5 - index;
+                        return (
+                            <label key={index} className={ratingValue <= rating ? "selected" : ""}>
+                                <input
+                                    type="radio"
+                                    name="rating"
+                                    value={ratingValue}
+                                    onClick={() => setRating(ratingValue)}
+                                />
+                                &#9733; {/* Star character */}
+                            </label>
+                        );
+                    })}
+                </div>
+                <TextField
+                    placeholder={"Write your review..."}
+                    className="review-input"
+                    multiline={true}
+                    onChange={handleTextChange}
+                    value={review}
+                />
+                {errorMessage && <div className="error-message">{errorMessage}</div>}
+                <Button variant="contained"
+                        type="submit"
+                        color="primary"
+                        disabled={!(review && rating) || errorMessage !== ''}>
+                    Submit Review
+                </Button>
+            </form>
+            <Mention onResultSelect={handleSelectedResult}
+                     isSearching={isSearching}
+                     setIsSearching={setIsSearching} />
+        </div>
     );
 });
 
